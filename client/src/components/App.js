6c321709617c7dddfbb748f5066de692e632e3dc
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-11-02 13:33:37 +0100

    add auto login to app component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,24 @@
-import '../style/App.css';
-import Login from '../pages/Login';
+import React, { useEffect, useState } from "react";
+import Login from "../pages/Login";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  if (!user) return <Login onLogin={setUser} />;
+
   return (
-    <div className="App">
-      <header>
-        <h1>DOG ADOPTION AGENCY</h1>
-      </header>
-      <body>
-        <Login />
-      </body>
-    </div>
+    <>
+      <h1>{user}</h1>
+    </>
   );
 }
 

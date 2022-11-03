78e6dec41286ba746b0521dbc5de68ef89ddc880
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-11-03 10:48:34 +0100

    add link to dog card

--- client/src/components/DogCard.js
+++ client/src/components/DogCard.js
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
+
 function DogCard ({dog}){
-    const {name, breed, gender, image_url, age, size, user} = dog
+    const {id, name, breed, gender, image_url, age, size, user} = dog
     const {username} = user
     return(
         <>
-            <h4>{name} - Submitted by: {username}</h4>
+            <Link key={id} to={`/dogs/${id}`}>
+                <h4>{name} - Submitted by: {username}</h4>
+            </Link>
+            
         </>
     );
 }
--- client/src/pages/DogList.js
+++ client/src/pages/DogList.js
@@ -1,4 +1,3 @@
-import React, {useState, useEffect} from "react";
 import DogCard from "../components/DogCard";
 
 function DogList ({dogs}){

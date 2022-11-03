186662f0cedbdc0619b07d3255f92d412a1d5eba
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-11-03 10:12:22 +0100

    destructure dogCard component

--- client/src/components/DogCard.js
+++ client/src/components/DogCard.js
@@ -1,8 +1,9 @@
 function DogCard ({dog}){
     const {name, breed, gender, image_url, age, size, user} = dog
+    const {username} = user
     return(
         <>
-            <h4>{name} - Submitted by: {user.username}</h4>
+            <h4>{name} - Submitted by: {username}</h4>
         </>
     );
 }

b47bfd63a69e2bc8bb399783bc402edaed8dae7d
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-11-05 17:20:35 +0100

    add DogCard style

--- client/src/components/DogCard.js
+++ client/src/components/DogCard.js
@@ -1,6 +1,17 @@
 import { Link } from "react-router-dom";
+import Card from 'react-bootstrap/Card';
+
+export function truncate (string = '', limit = 0) {
+    if (string.length > 13) {
+        return `${string.substring(0, limit)}...`
+    } else{
+        return string
+    }
+}
 
 function DogCard ({dog, onDelete, onAdopt, currentUser}){
+    const textStyle = { fontSize: 15, color: 'black', fontWeight: 500}
+    const titleStyle = {fontSize: 30, color: 'black',  fontWeight: 'bold'}
     const {id, name, breed, gender, image_url, age, size, user, is_adopted} = dog
     const {username} = user
 
@@ -21,15 +32,20 @@ function DogCard ({dog, onDelete, onAdopt, currentUser}){
         .then((dog)=>onAdopt(dog))
     }
     return(
-        <>
+        <Card style={{ width: '100%', margin: '10px', }}>
             <Link key={id} to={`/dogs/${id}`}>
-                <h4>{name} - Submitted by: {username}</h4>
+                <Card.Img src={image_url} alt={name} style={{height: '15rem'}}/>
+                <Card.ImgOverlay >
+                    <div style={{background: 'white', opacity: 0.8, padding: '10px'}}>
+                        <Card.Title style={titleStyle}>{truncate(name, 12)}</Card.Title>
+                        <Card.Text style={textStyle}>Breed: {breed}%</Card.Text>
+                        <Card.Text style={textStyle}>Gender: {gender}%</Card.Text>
+                    </div>
+                </Card.ImgOverlay>
             </Link>
             <button onClick={handleDelete}>Delete</button>
             {is_adopted || username === currentUser ? null : <button onClick={handleUpdate}>Adopt</button>}
-            
-            
-        </>
+        </Card>  
     );
 }
 

bbab77f99e01a3e8e38e6e8d519a51b7f86844b5
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-11-05 19:55:51 +0100

    ADD STYLE TO MYDOGS COMPONENT

--- client/src/pages/DogList.js
+++ client/src/pages/DogList.js
@@ -5,7 +5,8 @@ import Container from 'react-bootstrap/Container';
 
 function DogList ({dogs, onDelete, onAdopt, currentUser}){
     const nonAdoptedDogs = dogs.filter((dog) => dog.is_adopted === false);
-    const displayDogs = nonAdoptedDogs.map(dog => <Col xs={12} sm={12} md={6} lg={6} xl={4} key={dog.id}>
+    const displayDogs = nonAdoptedDogs.map(dog => 
+        <Col xs={12} sm={12} md={6} lg={6} xl={4} key={dog.id}>
             <DogCard key={dog.id} dog = {dog} onDelete = {onDelete} onAdopt={onAdopt} currentUser = {currentUser}/> 
         </Col>
         );
--- client/src/pages/MyDogs.js
+++ client/src/pages/MyDogs.js
@@ -1,15 +1,32 @@
 import DogCard from "../components/DogCard";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 
 function MyDogs ({dogs, onDelete, onAdopt, currentUser}){
     const myAdoptedDogs = dogs.filter((dog) => dog.adopted_by === currentUser)
+    const displayMyAdoptedDogs = myAdoptedDogs.map((dog) =>
+        <Col xs={12} sm={12} md={6} lg={6} xl={4} key={dog.id}> 
+            <DogCard key={dog.id} dog = {dog} onDelete = {onDelete} onAdopt={onAdopt} currentUser = {currentUser}/>
+        </Col>
+        );
     const myDogs = dogs.filter((dog) => dog.user.username === currentUser)
+    const displayMyDogs = myDogs.map((dog) =>
+        <Col xs={12} sm={12} md={6} lg={6} xl={4} key={dog.id}> 
+            <DogCard key={dog.id} dog = {dog} onDelete = {onDelete} onAdopt={onAdopt} currentUser = {currentUser}/>
+            </Col>
+        );
     return (
-        <>
-            <h3>Dogs I submitted</h3>
-            {myDogs.map((dog) => <DogCard key={dog.id} dog = {dog} onDelete = {onDelete} onAdopt={onAdopt} currentUser = {currentUser}/>)}
-            <h3>Dogs I Adopted</h3>
-            {myAdoptedDogs.map((dog) => <DogCard key={dog.id} dog = {dog} onDelete = {onDelete} onAdopt={onAdopt} currentUser = {currentUser}/>)}
-        </>
+        <Container>
+            <Row>
+                <h3>Dogs I submitted</h3>
+                {displayMyDogs}
+            </Row>
+            <Row>
+                <h3>Dogs I Adopted</h3>
+                {displayMyAdoptedDogs}
+            </Row>
+        </Container>
         
     );
 }

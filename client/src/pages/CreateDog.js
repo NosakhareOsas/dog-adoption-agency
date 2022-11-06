6ce4df1b8018bed3fc36870f254fe151dc713ed0
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-11-06 02:37:25 +0100

    add style to create dog component

--- client/src/pages/CreateDog.js
+++ client/src/pages/CreateDog.js
@@ -1,4 +1,11 @@
 import React, { useState } from "react";
+import {Button, Card} from 'react-bootstrap';
+import Badge from 'react-bootstrap/Badge';
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 
 function CreateDog({onDogCreate}){
     let baseData = {
@@ -14,7 +21,7 @@ function CreateDog({onDogCreate}){
     const [isLoading, setIsLoading] = useState(false);
 
     function handleChange(e){
-        setFormData({...formData, [e.target.id]:e.target.value})
+        setFormData({...formData, [e.target.name]:e.target.value})
     }
 
     function handleSubmit(e) {
@@ -39,75 +46,87 @@ function CreateDog({onDogCreate}){
         });
       }
     return(
-        <>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="name">Name</label>
-                <input
-                    type="text"
-                    id="name"
-                    autoComplete="off"
-                    value={formData.name}
-                    onChange={handleChange}
-                />
-                <br/>
-                
-                <label htmlFor="breed">Breed</label>
-                <input
-                    type="text"
-                    id="breed"
-                    autoComplete="off"
-                    value={formData.breed}
-                    onChange={handleChange}
-                />
-                <br/>
-                
-                <label htmlFor="imageUrl">Image Link</label>
-                <input
-                    type="text"
-                    id="image_url"
-                    autoComplete="off"
-                    value={formData.image_url}
-                    onChange={handleChange}
-                />
-                <br/>
+        <Container>
+            <h1 style={{ margin: '80px' }}>SUBMIT YOUR DOG FOR ADOPTION, TODAY!!!</h1>
+            <Card style={{ padding: '25px' }}>
+                <Form onSubmit={handleSubmit}>
+                    <Row className="mb-3">
+                        <Form.Group as={Col} md="6" controlId="formBasicName"> 
+                            <FloatingLabel label="Enter Dog name">
+                                <Form.Control
+                                    type="text"
+                                    placeholder="name"
+                                    onChange={handleChange} 
+                                    name='name'
+                                    style={{ height: '50px' }}
+                                />
+                            </FloatingLabel>
+                        </Form.Group>
 
-                <label htmlFor="age">Age</label>
-                <input
-                    type="text"
-                    id="age"
-                    autoComplete="off"
-                    value={formData.age}
-                    onChange={handleChange}
-                />
-                <br/>
-                
-                <select id='size' onChange={handleChange} >
-                    <option>What is the size of your dog</option>
-                    <option value="Small">Small</option>
-                    <option value="Medium">Medium</option>
-                    <option value="Big">Big</option>
-                </select>
-                <br/>
+                        <Form.Group as={Col} md="6" controlId="formBasicBreed">
+                            <FloatingLabel label="Enter Dog Breed">
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Breed"
+                                    onChange={handleChange} 
+                                    name='breed'
+                                    style={{ height: '50px' }}
+                                />
+                            </FloatingLabel>
+                        </Form.Group>
+                    </Row>
 
-                <select id='gender' onChange={handleChange} >
-                    <option>What is the gender of your dog</option>
-                    <option value="Male">Male</option>
-                    <option value="Female">Female</option>
-                </select>
-                <br/>
+                    <Row className="mb-3">
+                        <Form.Group as={Col} md="6" controlId="formBasicImage">
+                            <FloatingLabel label="Enter Dog Image Link">
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Image"
+                                    onChange={handleChange} 
+                                    name='image_url'
+                                    style={{ height: '50px' }}
+                                />
+                            </FloatingLabel>
+                        </Form.Group>
 
-                
-                <button variant="fill" color="primary" type="submit">
-                    {isLoading ? "Loading..." : "Create Dog Profile"}
-                </button>
-                <br/>
-                
-                {errors.map((err) => (
-                    <h5 key={err}>{err}</h5>
-                ))}
-                <br/>
-            </form>
-        </>
+                        <Form.Group as={Col} md="6" controlId="formBasicAge">
+                            <FloatingLabel label="Enter Dog Age">
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Age"
+                                    onChange={handleChange} 
+                                    name='age'
+                                    style={{ height: '50px' }}
+                                />
+                            </FloatingLabel>
+                        </Form.Group>
+                    </Row>
+
+                    <Row className="mb-3">
+                        <Form.Group as={Col} md="6" controlId="formBasicSize">
+                            <Form.Select aria-label="Default select example" name='size' onChange={handleChange}>
+                                    <option>What is the size of your dog?</option>
+                                    <option value="Small">Small</option>
+                                    <option value="Medium">Medium</option>
+                                    <option value="Big">Big</option>
+                            </Form.Select>
+                        </Form.Group>
+
+                        <Form.Group as={Col} md="6" controlId="formBasicName">
+                            <Form.Select aria-label="Default select example" name='gender' onChange={handleChange}>
+                                    <option>What is the age of your dog?</option>
+                                    <option value="Male">Male</option>
+                                    <option value="Female">Female</option>
+                            </Form.Select>
+                        </Form.Group>
+                    </Row>
+                    <Button variant="primary" color="primary" type="submit" style={{ padding: '10px', margin: '10px' }}>
+                        {isLoading ? "Loading..." : "Create Dog Profile"}
+                    </Button>
+                </Form>
+                    {errors.map((err) => (<h5 key={err}>{err}</h5>))}
+            </Card> 
+        </Container>     
     );
 }
 

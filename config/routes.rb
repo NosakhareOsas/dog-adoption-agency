8b0ed6215748c9e6ddb016bdfd6c87efc358cbc8
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-11-06 20:17:12 +0100

    add namespace to route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :dogs, only: [:create, :show, :index, :destroy, :update]
-  # resources :users
-  post "/signup", to: "users#create"
-  get "/me", to: "users#show"
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
+  namespace :api do
+    resources :dogs, only: [:create, :show, :index, :destroy, :update]
+    post "/signup", to: "users#create"
+    get "/me", to: "users#show"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+  end
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end

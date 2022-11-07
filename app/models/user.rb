c74ecad81782c504ecf4e6a440ed5d6c027764f1
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-11-07 07:08:43 +0100

    add additional password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_many :dogs, dependent: :destroy
     has_secure_password
     validates :username, presence: true, uniqueness: true
+    validates :password, length: {minimum: 8}
 end
